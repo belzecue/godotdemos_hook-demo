6371ed4f262ac7fba09b03fb747ce3ff2609ba7a
OptionalDev <optionalstudio@gmail.com>
2020-04-07 20:26:21 +0200

    Add the pull of the chain

--- Player.gd
+++ Player.gd
@@ -9,8 +9,10 @@ const GRAVITY = 60				# Gravity applied every second
 const MAX_SPEED = 2000			# Maximum speed the player is allowed to move
 const FRICTION_AIR = 0.95		# The friction while airborne
 const FRICTION_GROUND = 0.85	# The friction while on the ground
+const CHAIN_PULL = 105
 
 var velocity = Vector2(0,0)		# The velocity of the player (kept over time)
+var chain_velocity := Vector2(0,0)
 var can_jump = false			# Whether the player used their air-jump
 
 
@@ -30,6 +32,26 @@ func _physics_process(_delta: float) -> void:
 
 	# Falling
 	velocity.y += GRAVITY
+
+	# Hook physics
+	if $Chain.hooked:
+		# `to_local($Chain.tip).normalized()` is the direction that the chain is pulling
+		chain_velocity = to_local($Chain.tip).normalized() * CHAIN_PULL
+		if chain_velocity.y > 0:
+			# Pulling down isn't as strong
+			chain_velocity.y *= 0.55
+		else:
+			# Pulling up is stronger
+			chain_velocity.y *= 1.65
+		if sign(chain_velocity.x) != sign(walk):
+			# if we are trying to walk in a different
+			# direction than the chain is pulling
+			# reduce its pull
+			chain_velocity.x *= 0.7
+	else:
+		# Not hooked -> no chain velocity
+		chain_velocity = Vector2(0,0)
+	velocity += chain_velocity
 
 	velocity.x += walk		# apply the walking
 	move_and_slide(velocity, Vector2.UP)	# Actually apply all the forces
